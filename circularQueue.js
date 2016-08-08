7a935542f0cfbc719ebdf46610b7fb20cc79c6ce
Stephen Mayeux <stephenmayeux@gmail.com>
2016-08-09 02:49:23 +0900

    hot potato game

--- circularQueue.js
+++ circularQueue.js
@@ -1,9 +1,24 @@
-var queue = require('./queue');
-
-var test = new queue();
-test.enqueue('steve');
-test.print();
+var Queue = require('./queue');
 
 function hotPotato(nameList, num) {
+  var queue = new Queue();
 
+  nameList.forEach(function(name) {
+    queue.enqueue(name);
+  });
+
+  var eliminated = '';
+
+  while (queue.size() > 1) {
+    for (var i = 0; i < num; i++) {
+      queue.enqueue(queue.dequeue());
+    }
+    eliminated = queue.dequeue();
+    console.log(eliminated + ' was eliminated!');
+  }
+  return queue.dequeue();
 }
+
+var names = ['Steve', 'Tanya', 'CJ', 'Keith', 'Kathrim', 'Charlie'];
+var winner = hotPotato(names, 7);
+console.log('The winner is ' + winner);
